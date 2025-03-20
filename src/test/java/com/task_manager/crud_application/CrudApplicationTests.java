b2ba524d571563a9f50e25001adf54039ec95d3e
navarathan <navarathan0926@gmail.com>
2025-03-20 21:23:31 +0530

    first commit

---
+++ src/test/java/com/task_manager/crud_application/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_manager.crud_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
